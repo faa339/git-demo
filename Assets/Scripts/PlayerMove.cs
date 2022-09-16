0aab9c663ba8831fbed65bd1b764d9a8f9015cba
Faris Ahmed T. Alotaibi <faa339@nyu.edu>
2022-09-16 15:12:23 -0400

    Starting playermove logic

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    float speed = 5f;
+    int counter = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Staring now!");
     }
 
     // Update is called once per frame
